c99ae894cc8d35474ac10a627084af230fb7764d
Gao Wang <gaow@uchicago.edu>
2019-10-09 09:13:28 -0500

    Add documentation

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,3 +1,3 @@
-var notebooksArray = ['VAT']
-var notebooksDict = {'Association-Analysis-of-Sequence-Data-using-Variant-Association-Tools-(VAT)-for-Complex-Traits-1': 'VAT'}
-var notebooksArrayMap = {'VAT': 'Association Analysis of ... mplex Traits'}
+var notebooksArray = ['PSEQ', 'VAT']
+var notebooksDict = {'Association-Analysis-of-Sequence-Data-using-PLINK-SEQ-(PSEQ)-1': 'PSEQ', 'Association-Analysis-of-Sequence-Data-using-Variant-Association-Tools-(VAT)-for-Complex-Traits-1': 'VAT'}
+var notebooksArrayMap = {'PSEQ': 'Association Analysis of ... K/SEQ (PSEQ)', 'VAT': 'Association Analysis of ... mplex Traits'}
