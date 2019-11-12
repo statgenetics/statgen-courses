f97f183b035fa00bb9c1ee192b10b77ddde11247
Gao Wang <gaow@uchicago.edu>
2019-11-12 17:37:07 -0600

    Add gemini to installation list

--- src/vm-setup.sh
+++ src/vm-setup.sh
@@ -1,15 +1,13 @@
+set -e
 # setup web server
-apt-get update & apt-get install -y nginx acl
+apt-get update && apt-get install -y nginx acl
 # setup docker
-curl -fsSL get.docker.com -o /tmp/get-docker.sh
-sh /tmp/get-docker.sh
+curl -fsSL get.docker.com -o /tmp/get-docker.sh && sh /tmp/get-docker.sh
 # setup SoS to run utility scripts
-curl -fsSL https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -o /tmp/miniconda.sh
-bash /tmp/miniconda.sh -bfp /usr/local
+curl -fsSL https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -o /tmp/miniconda.sh && bash /tmp/miniconda.sh -bfp /usr/local
 conda install -y -c conda-forge sos
 # utility script for running tutorials
-curl -fsSL https://raw.githubusercontent.com/statgenetics/statgen-courses/master/src/statgen-setup -o /usr/local/bin/statgen-setup
-chmod +x /usr/local/bin/statgen-setup
+curl -fsSL https://raw.githubusercontent.com/statgenetics/statgen-courses/master/src/statgen-setup -o /usr/local/bin/statgen-setup && chmod +x /usr/local/bin/statgen-setup
 # pull docker images
 statgen-setup update --tutorial vat pseq igv popgen regression annovar mlink slink gemini
 # add users
