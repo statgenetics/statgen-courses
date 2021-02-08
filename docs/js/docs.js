bac0678802d234e2e246254cbcfd46ecb12891f9
Gao Wang <wangow@gmail.com>
2021-02-07 21:40:18 -0500

    Update documentation

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,3 +1,3 @@
-var notebooksArray = ['PSEQ', 'VAT']
-var notebooksDict = {'Association-Analysis-of-Sequence-Data-using-PLINK-SEQ-(PSEQ)-1': 'PSEQ', 'Association-Analysis-of-Sequence-Data-using-Variant-Association-Tools-(VAT)-for-Complex-Traits-1': 'VAT'}
-var notebooksArrayMap = {'PSEQ': 'Association Analysis of ... K/SEQ (PSEQ)', 'VAT': 'Association Analysis of ... mplex Traits'}
+var notebooksArray = ['PSEQ', 'VAT', 'finemapping', 'finemapping_answers']
+var notebooksDict = {'Association-Analysis-of-Sequence-Data-using-PLINK-SEQ-(PSEQ)-1': 'PSEQ', 'Overview-1': 'finemapping', 'rm(list-ls())-1': 'finemapping_answers'}
+var notebooksArrayMap = {'PSEQ': 'Association Analysis of ... K/SEQ (PSEQ)', 'VAT': 'Overview', 'finemapping': 'Overview', 'finemapping_answers': 'rm(list=ls())'}
