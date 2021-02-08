298a6b18139b95693366017db18142620a334642
Gao Wang <wangow@gmail.com>
2021-02-07 21:44:57 -0500

    Add index page

--- docs/js/docs.js
+++
@@ -1,3 +0,0 @@
-var notebooksArray = ['PSEQ', 'VAT', 'finemapping', 'finemapping_answers']
-var notebooksDict = {'Association-Analysis-of-Sequence-Data-using-PLINK-SEQ-(PSEQ)-1': 'PSEQ', 'Overview-1': 'finemapping', 'rm(list-ls())-1': 'finemapping_answers'}
-var notebooksArrayMap = {'PSEQ': 'Association Analysis of ... K/SEQ (PSEQ)', 'VAT': 'Overview', 'finemapping': 'Overview', 'finemapping_answers': 'rm(list=ls())'}
