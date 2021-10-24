d224be2bd3fc40471864a31819b29838e68faadc
Gao Wang <wangow@gmail.com>
2021-10-24 11:42:40 -0400

    Update documentation

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,3 +1,3 @@
 var notebooksArray = ['PSEQ', 'VAT', 'finemapping', 'finemapping_answers']
-var notebooksDict = {'Association-Analysis-of-Sequence-Data-using-PLINK-SEQ-(PSEQ)-1': 'PSEQ', 'Overview-1': 'finemapping', 'rm(list-ls())-1': 'finemapping_answers'}
-var notebooksArrayMap = {'PSEQ': 'Association Analysis of ... K/SEQ (PSEQ)', 'VAT': 'Overview', 'finemapping': 'Overview', 'finemapping_answers': 'rm(list=ls())'}
+var notebooksDict = {'Association-Analysis-of-Sequence-Data-using-PLINK-SEQ-(PSEQ)-1': 'PSEQ', 'Overview-1': 'finemapping', 'rm(list-ls())-1': 'finemapping_answers', 'Polygenic-Risk-Score-Analysis-on-Height-GWAS-data-1': 'ldpred2_example'}
+var notebooksArrayMap = {'PSEQ': 'Association Analysis of ... K/SEQ (PSEQ)', 'VAT': 'Overview', 'finemapping': 'Overview', 'finemapping_answers': 'rm(list=ls())', 'ldpred2_example': 'Polygenic Risk Score An ... ht GWAS data'}
