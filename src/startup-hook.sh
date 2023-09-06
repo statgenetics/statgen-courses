7ec29bea4709bcf391b2db63cefb3d6bc934335f
Gao Wang <wangow@gmail.com>
2023-09-05 23:04:24 -0400

    Fix the build script

--- src/startup-hook.sh
+++ src/startup-hook.sh
@@ -15,7 +15,7 @@ function main() {
   # https://jupyter-docker-stacks.readthedocs.io/en/latest/using/common.html#startup-hooks
   mkdir -p /usr/local/bin/start-notebook.d
   # For large files, you can put README as a placeholder instead of the actual tutorial name, to skip pre-loading any files to it	
-  echo -e '#!/bin/bash\n/usr/local/bin/pull-tutorial.sh ${TUTORIAL_NAME}' > /usr/local/bin/start-notebook.d/get-updates.sh
+  echo -e "#!/bin/bash\n/usr/local/bin/pull-tutorial.sh ${TUTORIAL_NAME}" > /usr/local/bin/start-notebook.d/get-updates.sh
   chmod a+x /usr/local/bin/start-notebook.d/get-updates.sh
   # Users can type in "get-data" command in bash when they run the tutorial the first time, to download the data.
   cp /usr/local/bin/start-notebook.d/get-updates.sh /usr/local/bin/get-data
