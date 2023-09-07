be33a313b485b1a16eaf28eb2c4e2221d5b7e5ca
Gao Wang <wangow@gmail.com>
2023-09-06 20:49:19 -0400

    Update regenie docker image to delay large files update

--- docker/regenie.dockerfile
+++ docker/regenie.dockerfile
@@ -8,10 +8,10 @@ RUN mamba install -c conda-forge r-ggrepel r-qqman -y && mamba clean --all -f -y
 
 USER root
 RUN curl -sSo /tmp/startup-hook.sh https://raw.githubusercontent.com/statgenetics/statgen-courses/master/src/startup-hook.sh && \
-   /bin/bash /tmp/startup-hook.sh regenie && rm -f /tmp/startup-hook.sh
+   /bin/bash /tmp/startup-hook.sh regenie 1 && rm -f /tmp/startup-hook.sh
 
 USER jovyan
 
 # Download data to docker image
 RUN mkdir -p /home/jovyan/.work
-RUN cd /home/jovyan/.work && curl -fsSL https://statgen.us/files/2021/11/mwe_regenie.tar.gz -o mwe_regenie.tar.gz && tar -xzvf mwe_regenie.tar.gz && rm -rf *.tar.gz
+RUN cd /home/jovyan/.work && curl -fsSL https://statgen.us/files/2021/11/mwe_regenie.tar.gz -o mwe_regenie.tar.gz && tar -xzvf mwe_regenie.tar.gz && rm -rf *.tar.gz && rm -f /home/jovyan/.work/regenie_statgen_mwe/._*
--- src/startup-hook.sh
+++ src/startup-hook.sh
@@ -2,6 +2,7 @@
 
 GIT_BRANCH="master"
 TUTORIAL_NAME=${1}
+LARGE_FILE=${2}
 
 if [ -z "${TUTORIAL_NAME}" ] ; then
     echo Usage: ${0} \<turtorial name\>
@@ -15,10 +16,15 @@ function main() {
   # https://jupyter-docker-stacks.readthedocs.io/en/latest/using/common.html#startup-hooks
   mkdir -p /usr/local/bin/start-notebook.d
   # For large files, you can put README as a placeholder instead of the actual tutorial name, to skip pre-loading any files to it	
-  echo -e "#!/bin/bash\n/usr/local/bin/pull-tutorial.sh ${TUTORIAL_NAME}" > /usr/local/bin/start-notebook.d/get-updates.sh
+  if [ "$LARGE_FILE" -eq 0 ]; then
+  	echo -e "#!/bin/bash\n/usr/local/bin/pull-tutorial.sh ${TUTORIAL_NAME}" > /usr/local/bin/start-notebook.d/get-updates.sh
+  else
+  	echo -e "#!/bin/bash\n/usr/local/bin/pull-tutorial.sh README" > /usr/local/bin/start-notebook.d/get-updates.sh
+  fi 
   chmod a+x /usr/local/bin/start-notebook.d/get-updates.sh
   # Users can type in "get-data" command in bash when they run the tutorial the first time, to download the data.
-  cp /usr/local/bin/start-notebook.d/get-updates.sh /usr/local/bin/get-data
+  echo -e "#!/bin/bash\n/usr/local/bin/pull-tutorial.sh ${TUTORIAL_NAME}" > /usr/local/bin/get-data
+  chmod a+x /usr/local/bin/get-data
 }
 
 main
