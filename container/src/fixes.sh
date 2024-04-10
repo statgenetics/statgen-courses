19c407f7aaec96f305e50fee8b5c2a9d37e19212
gaow <wangow@gmail.com>
2024-04-09 22:59:30 -0400

    Update fixes.sh

--- container/src/fixes.sh
+++ container/src/fixes.sh
@@ -8,12 +8,8 @@ sed -i '2i export PYTHONPATH="/root/micromamba/envs/python_libs/lib/python3.12/s
 sed -i '2i export PYTHONPATH="/root/micromamba/envs/python_libs/lib/python3.12/site-packages"' /root/.pixi/bin/python3
 sed -i '2i export PYTHONPATH="/root/micromamba/envs/python_libs/lib/python3.12/site-packages"' /root/.pixi/bin/jupyter-lab
 sed -i '2i export PYTHONPATH="/root/micromamba/envs/python_libs/lib/python3.12/site-packages"' /root/.pixi/bin/jupyter-server
-
-sed -i '2i export R_LIBS="/root/micromamba/envs/r_libs/lib/R/library"' /root/.pixi/bin/r
-sed -i '2i export R_LIBS="/root/micromamba/envs/r_libs/lib/R/library"' /root/.pixi/bin/rscript
-sed -i '2i export R_LIBS="/root/micromamba/envs/r_libs/lib/R/library"' /root/.pixi/bin/jupyter-lab
-sed -i '2i export R_LIBS="/root/micromamba/envs/r_libs/lib/R/library"' /root/.pixi/bin/jupyter-server
 echo "unset PYTHONPATH" >> /root/.bashrc
+echo '.libPaths("~/micromamba/envs/r_libs/lib/R/library")' >> /root/.Rprofile
 
 # pixi global currently gives it wrappers all lowercase names, so we need to make symlinks for R and Rscript
 ln -sf /root/.pixi/bin/r /root/.pixi/bin/R
