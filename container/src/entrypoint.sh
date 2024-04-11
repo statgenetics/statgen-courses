4c7212411fa320e01e49550380da7e04bc9d0429
Yiwei Zhang <yiwei.zhang@memverge.com>
2024-04-11 13:56:58 -0700

    edit entrypoint.sh

--- container/src/entrypoint.sh
+++ container/src/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#curl -o /root/jn_entrypoint.sh https://raw.githubusercontent.com/statgenetics/statgen-courses/master/setup/MMCloud/jn_entrypoint.sh
+curl -o /root/jn_entrypoint.sh https://raw.githubusercontent.com/yiweizh-memverge/statgen-courses/master/setup/MMCloud/jn_entrypoint.sh
 chmod +x /root/jn_entrypoint.sh
 bash /root/jn_entrypoint.sh
 
