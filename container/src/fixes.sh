4d32ea72ff0ecd546c12e134baa07e2fb6feac68
Yiwei Zhang <yiwei.zhang@memverge.com>
2024-04-12 13:54:46 -0700

    fix plink.multivariate, cassi and missing r packages

--- container/src/fixes.sh
+++ container/src/fixes.sh
@@ -2,8 +2,11 @@
 # r-rgl needs libGL.so.1 provided by the system package manager
 apt-get -y install libgl1 libgomp1
 
-# install plink.multivariate
-curl -o /root/.pixi/bin/plink.multivariate https://github.com/statgenetics/statgen-courses/raw/master/handout/misc/plink.multivariate && chmod +x /root/.pixi/bin/plink.multivariate
+# install cASSI
+curl -o /root/cassi-v2.51-linux-x86_64.zip https://www.staff.ncl.ac.uk/richard.howey/cassi/cassi-v2.51-linux-x86_64.zip && unzip -d /root /root/cassi-v2.51-linux-x86_64.zip && cp /root/cassi-v2.51-linux-x86_64/cassi /usr/local/bin/ && chmod +x /usr/local/bin/cassi
+
+# clean up
+rm /root/cassi-v2.51-linux-x86_64.zip
 
 # temporary fix for https://github.com/vatlab/jupyterlab-sos/issues/72
 eval "$(micromamba shell hook --shell bash)"
