51fb5b0ac114998b9ae0b0993596ac162fc7c60b
gaow <wangow@gmail.com>
2024-04-12 19:15:25 -0400

    Update init.sh

--- container/src/init.sh
+++ container/src/init.sh
@@ -3,8 +3,6 @@
 set -e
 
 # Only show PYTHONPATH and R_LIBS to specific executables
-sed -i '2i export JUPYTER_PATH="/root/micromamba/envs/python_libs/share/jupyter"' /root/.pixi/bin/jupyter-lab
-sed -i '2i export JUPYTER_PATH="/root/micromamba/envs/python_libs/share/jupyter"' /root/.pixi/bin/jupyter-server
 sed -i '2i export PYTHONPATH="/root/micromamba/envs/python_libs/lib/python3.12/site-packages"' /root/.pixi/bin/python
 sed -i '2i export PYTHONPATH="/root/micromamba/envs/python_libs/lib/python3.12/site-packages"' /root/.pixi/bin/python3
 sed -i '2i export PYTHONPATH="/root/micromamba/envs/python_libs/lib/python3.12/site-packages"' /root/.pixi/bin/jupyter-lab
@@ -13,8 +11,13 @@ echo "unset PYTHONPATH" >> /root/.bashrc
 echo '.libPaths("~/micromamba/envs/r_libs/lib/R/library")' >> /root/.Rprofile
 
 # pixi global currently gives it wrappers all lowercase names, so we need to make symlinks for R and Rscript
-ln -sf /root/.pixi/bin/r /root/.pixi/bin/R
-ln -sf /root/.pixi/bin/rscript /root/.pixi/bin/Rscript
+if [ ! -e "${HOME}/.pixi/bin/R" ]; then
+  ln -sf "${HOME}/.pixi/bin/r" "${HOME}/.pixi/bin/R"
+fi
+
+if [ ! -e "${HOME}/.pixi/bin/Rscript" ]; then
+  ln -sf "${HOME}/.pixi/bin/rscript" "${HOME}/.pixi/bin/Rscript"
+fi
 
 # Register Juypter kernels
 find /root/micromamba/envs/python_libs/share/jupyter/kernels/ -maxdepth 1 -mindepth 1 -type d | \
