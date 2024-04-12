5ce007e71335b976c1fa5809fc60dd0f333b2144
gaow <wangow@gmail.com>
2024-04-12 06:51:36 -0400

    Update fixes.sh

--- container/src/fixes.sh
+++ container/src/fixes.sh
@@ -5,5 +5,9 @@ apt-get -y install libgl1 libgomp1
 # install plink.multivariate
 curl -o /root/.pixi/bin/plink.multivariate https://github.com/statgenetics/statgen-courses/raw/master/handout/misc/plink.multivariate && chmod +x /root/.pixi/bin/plink.multivariate
 
+# temporary fix for https://github.com/vatlab/jupyterlab-sos/issues/72
+micromamba activate python_libs
+pip install jupyterlab-sos --force-reinstall
+
 # pixi global mistakenly points the samtools wrapper to samtools.pl, so we need to revert this change
 #sed -i "s/samtools.pl/samtools/" /root/.pixi/bin/samtools
