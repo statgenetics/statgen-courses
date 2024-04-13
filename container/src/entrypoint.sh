aa0e4ae68da562e020d9c72bc8d3d64aada0f9ae
gaow <wangow@gmail.com>
2024-04-13 15:23:48 -0400

    Update entrypoint.sh

--- container/src/entrypoint.sh
+++ container/src/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -o /root/jn_entrypoint.sh https://raw.githubusercontent.com/yiweizh-memverge/statgen-courses/master/setup/MMCloud/jn_entrypoint.sh
-chmod +x /root/jn_entrypoint.sh
-bash /root/jn_entrypoint.sh
+curl -o /tmp/jn_entrypoint.sh https://raw.githubusercontent.com/yiweizh-memverge/statgen-courses/master/setup/MMCloud/jn_entrypoint.sh
+chmod +x /tmp/jn_entrypoint.sh
+bash /tmp/jn_entrypoint.sh
 
 cd /root/statgen-courses/
 jupyter-lab
